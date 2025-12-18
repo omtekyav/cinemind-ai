41dee52d54b2527855d78ad83e39ae0ff7883370
omtekyav <174030142+omtekyav@users.noreply.github.com>
2025-12-18 18:39:00 +0300

    chore: setup project dependencies, config and testing infrastructure

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -5,10 +5,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str = "CineMind AI"
     DEBUG: bool = True
-    TMDB_API_KEY: str
+    TMDB_API_KEY: str 
     TMDB_BASE_URL: str = "https://api.themoviedb.org/3"
-    SENTIMENT_SERVICE_URL: str = "http://localhost:8001"
 
+    #sentiment service config
+    SENTIMENT_SERVICE_URL: str = "http://localhost:8001"
+    #env dosyası okuma aracı
     model_config = SettingsConfigDict(env_file=".env")
 
 @lru_cache()
